a06b3cce879e050283bdb9d448f55f506097c927
HermannChoi <hiyunseok347@gmail.com>
2023-10-19 15:03:00 +0900

    Done

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      let overlay = document.querySelector(".overlay");
+      overlay.style.display = "none";
+    }, 3000);
+  });
+  
